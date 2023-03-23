cac4ebe7ca5bda5fd489639ceb211076ae538645
Arunabh Das <arunabhdas@gmail.com>
2023-03-23 06:37:30 -0400

    Scaffolded using npm create skeleton-app@latest CodeLabsProKit

---
+++ codelabsprokit/src/routes/+layout.svelte
@@ -0,0 +1,49 @@
+<script lang='ts'>
+	// The ordering of these imports is critical to your app working properly
+	import '@skeletonlabs/skeleton/themes/theme-skeleton.css';
+	// If you have source.organizeImports set to true in VSCode, then it will auto change this ordering
+	import '@skeletonlabs/skeleton/styles/all.css';
+	// Most of your app wide CSS should be put in this file
+	import '../app.postcss';
+	import { AppShell, AppBar } from '@skeletonlabs/skeleton';
+</script>
+
+<!-- App Shell -->
+<AppShell>
+	<svelte:fragment slot="header">
+		<!-- App Bar -->
+		<AppBar>
+			<svelte:fragment slot="lead">
+				<strong class="text-xl uppercase">CodeLabsProKit</strong>
+			</svelte:fragment>
+			<svelte:fragment slot="trail">
+				<a
+					class="btn btn-sm variant-ghost-surface"
+					href="#"
+					target="_blank"
+					rel="noreferrer"
+				>
+					Home
+				</a>
+				<a
+					class="btn btn-sm variant-ghost-surface"
+					href="#"
+					target="_blank"
+					rel="noreferrer"
+				>
+					Contact
+				</a>
+				<a
+					class="btn btn-sm variant-ghost-surface"
+					href="#"
+					target="_blank"
+					rel="noreferrer"
+				>
+					About
+				</a>
+			</svelte:fragment>
+		</AppBar>
+	</svelte:fragment>
+	<!-- Page Route Content -->
+	<slot />
+</AppShell>
