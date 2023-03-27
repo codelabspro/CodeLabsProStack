11edf14b3410fd6b4332777703734609a67b1ad3
Arunabh Das <arunabhdas@gmail.com>
2023-03-27 07:42:46 -0400

    Implemented login

--- CodeLabsProStack-backend/router/authentication.py
+++ CodeLabsProStack-backend/router/authentication.py
@@ -9,8 +9,8 @@ router = APIRouter(
 )
 
 @router.post('/login')
-def login(request: schemas.Login, db: Session = Depends(database.get_db())):
-    user = db.query(models.User).filter(models.User.email == request.username)
+def login(request: schemas.Login, db: Session = Depends(database.get_db)):
+    user = db.query(models.User).filter(models.User.email == request.username).first()
     if not user:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Invalid Credentials")
     return user
