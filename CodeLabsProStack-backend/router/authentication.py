083bcaf19bf2aa0e6112715869f3feb5b21372fc
Arunabh Das <arunabhdas@gmail.com>
2023-03-28 12:05:43 -0400

    Getting create_access_token working

--- CodeLabsProStack-backend/router/authentication.py
+++ CodeLabsProStack-backend/router/authentication.py
@@ -5,7 +5,7 @@ from fastapi import APIRouter, Depends, status, HTTPException, Response
 import schemas, models, database
 from hashing import Hash
 from sqlalchemy.orm import Session
-import token
+import accesstoken
 
 
 
@@ -22,7 +22,7 @@ def login(request: schemas.Login, db: Session = Depends(database.get_db)):
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Incorrect password")
 
     # generate jwt token and return
-    access_token = token.create_access_token(data={"sub": user.email})
+    access_token = accesstoken.create_access_token(data={"sub": user.email})
     return {"access_token": access_token, "token_type": "bearer"}
 
 
