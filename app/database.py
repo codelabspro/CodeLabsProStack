643830c91955fce8925e9561275340a3fe8c143c
Arunabh Das <arunabhdas@gmail.com>
2023-03-29 05:08:01 -0400

    Improved variable names in env

--- app/database.py
+++ app/database.py
@@ -11,13 +11,13 @@ dotenv_path = join(dirname(__file__), '.env')
 load_dotenv(dotenv_path)
 
 # SQLALCHEMY_SQLITE_URL = "sqlite:///./blog.db"
-POSTGRES_USER = os.environ.get("POSTGRES_USER")
+POSTGRES_USERNAME = os.environ.get("POSTGRES_USERNAME")
 POSTGRES_PASSWORD = os.environ.get("POSTGRES_PASSWORD")
 POSTGRES_HOSTNAME = os.environ.get("POSTGRES_HOSTNAME", "127.0.0.1")
 POSTGRES_PORT = os.environ.get("POSTGRES_PORT", 5432)
 POSTGRES_DATABASE = os.environ.get("POSTGRES_DATABASE", "")
 # SQLALCHEMY_DATABASE_URL = "postgresql://user:password@postgresserver/db"
-SQLALCHEMY_DATABASE_URL = f"postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_HOSTNAME}:{POSTGRES_PORT}/{POSTGRES_DATABASE}"
+SQLALCHEMY_DATABASE_URL = f"postgresql://{POSTGRES_USERNAME}:{POSTGRES_PASSWORD}@{POSTGRES_HOSTNAME}:{POSTGRES_PORT}/{POSTGRES_DATABASE}"
 
 #engine = create_engine(
 #    SQLALCHEMY_SQLITE_URL, connect_args={"check_same_thread": False}
