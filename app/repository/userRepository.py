243c9843fa7ae703adf5168cfdb2f366c0a2edac
Arunabh Das <arunabhdas@gmail.com>
2023-03-29 06:46:17 -0400

    Minor fix to userRepository create

--- app/repository/userRepository.py
+++ app/repository/userRepository.py
@@ -13,7 +13,7 @@ def get_all(db: Session):
     return users
 
 def create(request: models.UserCreate, db: Session):
-    new_user = models.User(name = request.name, email = request.email, password = Hash.bcrypt(request.password))
+    new_user = models.UserBase(name = request.name, email = request.email, password = Hash.bcrypt(request.password))
     db.add(new_user)
     db.commit()
     db.refresh(new_user)
