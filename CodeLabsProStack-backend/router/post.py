6f33eb437d06136b817986381a418a17f4c79087
Arunabh Das <arunabhdas@gmail.com>
2023-03-28 21:02:49 -0400

    Protecting Post routes with auth

--- CodeLabsProStack-backend/router/post.py
+++ CodeLabsProStack-backend/router/post.py
@@ -15,23 +15,23 @@ router = APIRouter(
 ## Post
 
 @router.get('/posts', response_model=List[schemas.ShowPost])
-def all(db: Session = Depends(database.get_db), get_current_user:schemas.User = Depends(oauth2.get_current_user)):
+def all(db: Session = Depends(database.get_db), current_user:schemas.User = Depends(oauth2.get_current_user)):
     return postRepository.get_all(db)
 
 @router.post('/post', status_code=status.HTTP_201_CREATED)
-def create(request: schemas.Post, db: Session = Depends(database.get_db)):
+def create(request: schemas.Post, db: Session = Depends(database.get_db), current_user:schemas.User = Depends(oauth2.get_current_user)):
     return postRepository.create(request, db)
 
 @router.delete('/post/{id}', status_code=200)
-def destroy(id: int, db: Session = Depends(database.get_db)):
+def destroy(id: int, db: Session = Depends(database.get_db), current_user:schemas.User = Depends(oauth2.get_current_user)):
     return postRepository.destroy(id, db)
 
 @router.put('/post/{id}', status_code=status.HTTP_202_ACCEPTED)
-def update(id: int, request: schemas.Post, db: Session = Depends(database.get_db)):
+def update(id: int, request: schemas.Post, db: Session = Depends(database.get_db), current_user:schemas.User = Depends(oauth2.get_current_user)):
     return postRepository.update(id, request, db)
 
 @router.get('/post/{id}', status_code=200, response_model=schemas.ShowPost)
-def show(id: int, response: Response, db: Session = Depends(database.get_db)):
+def show(id: int, response: Response, db: Session = Depends(database.get_db), current_user:schemas.User = Depends(oauth2.get_current_user)):
     return postRepository.show(id, response, db)
 
 ###############################################################################
